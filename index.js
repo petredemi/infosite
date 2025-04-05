d9187e8f2009a87dd80b3f1c4de1105a1f21c2e5
petrudemian <petredemi@gmail.com>
2025-04-05 18:37:24 +0100

    change port to 3000

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 var http = require('http');
 var url = require('url');
 var fs = require('fs');
-
-
 http.createServer(function (req, res) {
   var q = url.parse(req.url, true);
   var filename = "." + q.pathname;
@@ -15,4 +13,4 @@ http.createServer(function (req, res) {
     res.write(data);
     return res.end();
   });
-}).listen(8080);
+}).listen(3000);
