f71cc99e7e7be611ca139d3ae868435b1afb8d97
petrudemian <petredemi@gmail.com>
2025-04-06 15:22:22 +0100

    add first page index.html

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 
-let http = require('http');
-let url = require('url');
-let fs = require('fs');
-
-
+var http = require('http');
+var formidable = require('formidable');
 
 http.createServer(function (req, res) {
-  fs.readFile('.', function(err, data) {
+  if (req.url == '/fileupload') {
+    var form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files) {
+      res.write('File uploaded');
+      res.end();
+    });
+  } else {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
+    res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
+    res.write('<input type="file" name="filetoupload"><br>');
+    res.write('<input type="submit">');
+    res.write('</form>');
     return res.end();
-  });
+  }
 }).listen(8080);
-
